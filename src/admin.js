3747f07b4ee5919579b49384e63266a58b53af32
tianziyihao <742406475@qq.com>
2018-08-31 22:45:31 +0800

    左侧菜单实现

--- src/admin.js
+++ src/admin.js
@@ -3,14 +3,15 @@ import { Row, Col } from 'antd';
 import NavLeft from './components/NavLeft'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import './style/common.less' 
 export default class Admin extends React.Component{
     render(){
         return (
             <Row className="container">
-                <Col span="3" className="nav-left">
+                <Col span="4" className="nav-left">
                     <NavLeft></NavLeft>
                 </Col>
-                <Col span="21" className="main">
+                <Col span="20" className="main">
                     <Header >
                     </Header>
                     <Row className="content">
