8d01698fbc834b28f38a3f706b095fcdd3dd3abf
wf <742406475@qq.com>
2018-09-15 15:32:38 +0800

    antd汉化

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 import ReactDOM from 'react-dom';
 import Router from './router'
 import './index.css';
@@ -7,8 +9,13 @@ import configureStore from './redux/store';
 import registerServiceWorker from './registerServiceWorker';
 const store = configureStore();
 
-ReactDOM.render(<Provider store={store}>
-        <Router />
-</Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <LocaleProvider locale={zhCN}>
+    <Provider store={store}>
+            <Router />
+    </Provider>
+  </LocaleProvider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
