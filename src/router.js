320727f9a1d5e64dbeb449525c1ed8178f4852cb
wf <742406475@qq.com>
2018-09-15 15:25:07 +0800

    路由404添加

--- src/router.js
+++ src/router.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import App from './App'
 import Test from './pages/demo'
-import { HashRouter, Route} from 'react-router-dom'
+import Page404 from './pages/404'
+import { HashRouter, Route, Switch} from 'react-router-dom'
 
 export default class EPRouter extends React.Component{
     render(){
         return(
             <HashRouter>
                 <App>
+                    <Switch>
                     <Route path="/test" component={Test}/>
+                    <Route exact component={Page404}/>
+                    </Switch>
                 </App>
             </HashRouter>
         )
