339f871ffadc084d82327a51a5dbc0aa42d5e463
wf <742406475@qq.com>
2018-09-15 15:34:19 +0800

    test修改

--- src/pages/demo/index.js
+++ src/pages/demo/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './index.less'
+import { Card, Icon, Avatar, Divider } from 'antd';
+
+const { Meta } = Card;
 
 class Test extends React.Component {
-    render() {
-        return (
-                <h1>come on</h1>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>come on</h1>
+        <Card
+          style={{ width: 300 }}
+          cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+          actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+        >
+          <Meta
+            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+            title="Card title"
+            description="This is the description"
+          />
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default Test;
